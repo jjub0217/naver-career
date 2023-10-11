0c98eadd02c460c1b4c9e4eacf70c63a0d4266ef
jjub0217 <noonssub1004@gmail.com>
2023-10-11 20:42:21 +0900

    section-search 버튼 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,87 +41,93 @@ $(function () {
   })
 
 
-  $('.section-search .btn-search.mo').click(function(){
+  $('.section-search .btn-search.mo button').click(function(){
     $('.section-search-modal').addClass('on')
     list()
   })
 
+  function list() {
 
-  let test = 'test'
+    fetch('./data.json')
+    .then(res => res.json())
+    .then(json => {
+      data = json.items
 
-
-
-    function list() {
-
-      fetch('./data.json')
-        .then(res => res.json())
-        .then(json => {
-          data = json.items
-
-          let html1 = ``
-          data.forEach(element => {
-            html1 += `
-              <li class="filter-item">
-                <button class="filter-item-title">${element.filterItemTitle}</button>
-                <ul class="depth1-sub-list">
-                  <li class="depth1-sub-item">
-                  <input type="hidden" value="all" name="all" />
-                  <button class="depth1-sub-item-all">
-                    전체
-                  </button>
-                </li>`
-              element.depth1SubList.forEach(el => {
-                html1 += `
-                  <li class="depth1-sub-item">
-                    <button class="depth1-sub-item-title">
-                      ${el.depth1SubItemTitle}
-                    </button>
-                    <ul class="depth2-sub-list">
-                  `
-                  el.depth2SubItemTitles.forEach(title =>{
-                    html1 += `
-                      <li class="depth2-sub-item">
-                       <input type="hidden" value="${title}" name="${title}" />
-                        <button class="depth2-sub-item-title">${title}</button>
-                      </li>`
-                  })
-                html1 += `
-                  </ul>
-                </li>
-                `
-              }) 
-              html1 += `
-                </ul>
-              </li>
-              `
-            })
-            $(".filter-list1").html(html1)
-            $('.filter-item-title').click(function () {
-              $(this).toggleClass('on');
-              $(this).siblings('.depth1-sub-list').toggleClass('on');
-            });
-            $('.depth1-sub-item').click(function(e){
-                e.stopPropagation()
-              if($(this).find('.depth2-sub-list').length){
-                $(this).find('.depth1-sub-item-title').toggleClass('on')
-                $(this).find('.depth2-sub-list').addClass('on')
-              }
-            })
-            $('.depth2-sub-item-title').click(function(e){
-                e.stopPropagation()
-              $(this).toggleClass('on')
-            })
-            $('.depth1-sub-item-all').click(function(e){
-                e.stopPropagation()
-              $(this).toggleClass('on')
-            })
+      let html1 = ``
+      data.forEach(element => {
+        html1 += 
+        `<li class="filter-item">
+          <button class="filter-item-title">${element.filterItemTitle}</button>
+          <ul class="depth1-sub-list">
+            <li class="depth1-sub-item">
+              <input type="hidden" value="all" name="all" />
+              <button class="depth1-sub-item-all">전체</button>
+            </li>`
+        element.depth1SubList.forEach(el => {
+          html1 += 
+          `<li class="depth1-sub-item">
+              <button class="depth1-sub-item-title">
+                ${el.depth1SubItemTitle}
+              </button>
+              <ul class="depth2-sub-list">`
+          el.depth2SubItemTitles.forEach(title =>{
+            html1 += 
+            `<li class="depth2-sub-item">
+              <input type="hidden" value="${title}" name="${title}" />
+              <button class="depth2-sub-item-title">${title}</button>
+            </li>`
+          })
+          html1 += 
+          `</ul>
+          </li>`
+        }) 
+        html1 += 
+        `</ul>
+        </li>`
       })
-    }
+      $(".filter-list1").html(html1);
+      $('.filter-item-title').click(function () {
+        $(this).toggleClass('on');
+        $(this).siblings('.depth1-sub-list').toggleClass('on');
+      });
+      $('.depth1-sub-item').click(function(e){
+        e.stopPropagation()
+        if($(this).find('.depth2-sub-list').length){
+          $(this).find('.depth1-sub-item-title').toggleClass('on')
+          $(this).find('.depth2-sub-list').toggleClass('on')
+        }
+      });
+      $('.depth2-sub-item-title').click(function(e){
+        e.stopPropagation()
+        $(this).toggleClass('on')
+      });
+      $('.depth1-sub-item').click(function(e){
+        e.stopPropagation()
+        if($(this).find(".depth1-sub-item-all").length){
+          $(this).find(".depth1-sub-item-all").toggleClass('on')
+          if($(this).find(".depth1-sub-item-all").hasClass('on')){
+            $(this).siblings((".depth1-sub-item")).find(".depth2-sub-item-title").addClass("on")
+          }else{
+            $(this).siblings((".depth1-sub-item")).find(".depth2-sub-item-title").removeClass("on")
+          }
+        }  
+      })
+    })
+
+  }
+  $(".category").click(function(){
+    $(this).toggleClass("on")
+  })
+  
+  $(".clear-filters").click(function(){
+    $(".depth1-sub-item-all").removeClass("on")
+    $(".depth2-sub-item-title").removeClass("on")
+    $(".category").removeClass("on")
+  })
 
   $('.section-search-modal .modal-close').click(function(){
     $('.section-search-modal').removeClass('on')
   })
-
 
   // pc 에서는 손가락으로 슬라이드가 움직여지면 안된다. 그렇기 때문에 옵션을 줘야한다. ===> touchRatio
   const visualSlide = new Swiper('.section-visual .swiper',{
